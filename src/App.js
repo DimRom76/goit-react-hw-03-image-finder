851fba37dcfe356ec5fbfe1549a7dca0b2dad175
Dmitriy Kotulsky <66743608+DimRom76@users.noreply.github.com>
2021-01-22 21:21:19 +0200

    перестроил компоненты

--- src/App.js
+++ src/App.js
@@ -3,15 +3,65 @@ import { Component } from 'react';
 import './App.css';
 import Searchbar from './Components/Searchbar';
 import ImageGallery from './Components/ImageGallery';
+import Button from './Components/Button/';
+import Loader from './Components/Loader/';
+
+import fetchPhoto from './Service/apiService';
+import STATUS from './Service/apiStatus';
 
 class App extends Component {
   constructor() {
     super();
-    this.state = { page: 0, queryString: '' };
+    this.state = {
+      page: 0,
+      queryString: '',
+      images: [],
+      status: '',
+      error: null,
+    };
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const { queryString: prevQueryString, page: prevPage } = prevState;
+    const { queryString: nextQueryString, page: nextPage } = this.state;
+
+    if (prevQueryString !== nextQueryString || prevPage !== nextPage) {
+      this.setState({ status: STATUS.PENDING });
+
+      this.getPhoto(nextQueryString, nextPage);
+    }
+  }
+
+  getPhoto(nextQueryString, nextPage) {
+    fetchPhoto(nextQueryString, nextPage)
+      .then(data => {
+        const arrayImages = [];
+        if (data.hits.length === 0) {
+          return Promise.reject(new Error(`Данных для отображения нет`));
+        }
+
+        data.hits.forEach(element => {
+          const { id, webformatURL, largeImageURL, tags } = element;
+          arrayImages.push({ id, webformatURL, largeImageURL, tags });
+        });
+        return arrayImages;
+      })
+      .then(arrayImages => {
+        this.setState({
+          images: [...this.state.images, ...arrayImages],
+          status: STATUS.RESOLVED,
+        });
+
+        window.scrollTo({
+          top: document.documentElement.scrollHeight,
+          behavior: 'smooth',
+        });
+      })
+      .catch(error => this.setState({ error, status: STATUS.REJECTED }));
   }
 
   onSubmit = values => {
-    this.setState({ page: 1, queryString: values });
+    this.setState({ page: 1, queryString: values, images: [] });
   };
 
   onClickButtomLoadMore = event => {
@@ -19,16 +69,23 @@ class App extends Component {
   };
 
   render() {
-    const { queryString, page } = this.state;
+    const { error, images, status } = this.state;
 
     return (
       <div className="App">
         <Searchbar onSubmit={this.onSubmit} />
-        <ImageGallery
-          queryString={queryString}
-          page={page}
-          onClick={this.onClickButtomLoadMore}
-        />
+
+        {status === STATUS.REJECTED && <h1>{error.message}</h1>}
+
+        {(status === STATUS.RESOLVED || status === STATUS.PENDING) && (
+          <ImageGallery images={images} />
+        )}
+
+        {status === STATUS.RESOLVED && (
+          <Button onClick={this.onClickButtomLoadMore} />
+        )}
+
+        {status === STATUS.PENDING && <Loader />}
       </div>
     );
   }
--- src/Components/ImageGallery/ImageGallery.js
+++ src/Components/ImageGallery/ImageGallery.js
@@ -1,71 +1,17 @@
 import { Component } from 'react';
 import PropTypes from 'prop-types';
 
-import fetchPhoto from '../../Service/apiService';
 import ImageGalleryItem from '../ImageGalleryItem';
 import Modal from '../Modal/Modal';
-import Button from '../Button/';
-import Loader from '../Loader/';
 
 import s from './ImageGallery.module.css';
 
-const Status = {
-  PENDING: 'pending',
-  RESOLVED: 'resolved',
-  REJECTED: 'rejected',
-};
 const bigImage = { alt: '', src: '' };
 
 class ImageGallery extends Component {
   constructor() {
     super();
-    this.state = { images: [], status: '', error: null, showModal: false };
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    const prevQueryString = prevProps.queryString;
-    const nextQueryString = this.props.queryString;
-
-    const prevPage = prevProps.page;
-    const nextPage = this.props.page;
-
-    if (prevQueryString !== nextQueryString || prevPage !== nextPage) {
-      this.setState({ status: Status.PENDING });
-
-      this.getPhoto(prevQueryString, nextQueryString, nextPage);
-    }
-  }
-
-  getPhoto(prevQueryString, nextQueryString, nextPage) {
-    fetchPhoto(nextQueryString, nextPage)
-      .then(data => {
-        const arrayImages = [];
-        if (data.hits.length === 0) {
-          return Promise.reject(new Error(`Данных для отображения нет`));
-        }
-
-        data.hits.forEach(element => {
-          const { id, webformatURL, largeImageURL, tags } = element;
-          arrayImages.push({ id, webformatURL, largeImageURL, tags });
-        });
-        return arrayImages;
-      })
-      .then(arrayImages => {
-        let newArrayImage;
-        if (prevQueryString === nextQueryString) {
-          newArrayImage = [...this.state.images, ...arrayImages];
-        } else {
-          newArrayImage = [...arrayImages];
-        }
-
-        this.setState({ images: newArrayImage, status: Status.RESOLVED });
-
-        window.scrollTo({
-          top: document.documentElement.scrollHeight,
-          behavior: 'smooth',
-        });
-      })
-      .catch(error => this.setState({ error, status: Status.REJECTED }));
+    this.state = { showModal: false };
   }
 
   toggleModal = () => {
@@ -79,21 +25,14 @@ class ImageGallery extends Component {
   };
 
   render() {
-    const { images, status, error, showModal } = this.state;
-
-    if (status === Status.REJECTED) {
-      return <h1>{error.message}</h1>;
-    }
+    const { showModal } = this.state;
+    const { images } = this.props;
 
     return (
       <>
         <ul className={s.ImageGallery}>
           <ImageGalleryItem images={images} onClickImage={this.onClickImage} />
         </ul>
-
-        {status === Status.RESOLVED && <Button onClick={this.props.onClick} />}
-
-        {status === Status.PENDING && <Loader />}
 
         {showModal && (
           <Modal onClose={this.toggleModal}>
@@ -106,9 +45,7 @@ class ImageGallery extends Component {
 }
 
 ImageGallery.propTypes = {
-  queryString: PropTypes.string.isRequired,
-  page: PropTypes.number.isRequired,
-  onClick: PropTypes.func.isRequired,
+  images: PropTypes.array,
 };
 
 export default ImageGallery;
